6a2218d2e90e40f795181ea017a8067bef2ee041
oligregz <gregorysak90@gmail.com>
2023-07-13 10:24:26 -0300

    feat: third challenge implemented

--- src/supera_challengers_1_4/Application.java
+++ src/supera_challengers_1_4/Application.java
@@ -2,6 +2,7 @@ package supera_challengers_1_4;
 
 import supera_challengers_1_4.first_challenger.OddPairs;
 import supera_challengers_1_4.second_challenger.ValueBanknotes;
+import supera_challengers_1_4.third_challenger.EqualityInDiference;
 
 public class Application {
 
@@ -20,5 +21,14 @@ public class Application {
     ValueBanknotes valueBanknotes = new ValueBanknotes();
     valueBanknotes.setAmount(576.73);
     valueBanknotes.printNotesAndCoins();
+
+    /* Third challenge */
+    System.out.println("--------------------------------------------------------------");
+    System.out.println("3º)");
+    EqualityInDiference equalityInDiference = new EqualityInDiference();
+    equalityInDiference.setDefaultValues();
+    equalityInDiference.getEqualityDiferencePairs(2);
+    System.out.println("Vezes em que a diferença entre pares são iguais ao valor alvo: "
+    + equalityInDiference.getVerify());
   }
 }
--- src/supera_challengers_1_4/third_challenger/EqualityInDiference.java
+++ src/supera_challengers_1_4/third_challenger/EqualityInDiference.java
@@ -1,5 +1,52 @@
 package supera_challengers_1_4.third_challenger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EqualityInDiference {
 
+  protected int size;
+  protected int verify;
+  protected List<Integer> numbers;
+
+
+  public int getSize() {
+    return size;
+  }
+ 
+  public void setSize(int size) {
+    this.size = size;
+  }
+
+  public int getVerify() {
+    return verify;
+  }
+
+  public List<Integer> getValues() {
+    return numbers;
+  }
+
+  public void setDefaultValues() {
+    numbers  = new ArrayList<>();
+    numbers.add(1);
+    numbers.add(5);
+    numbers.add(3);
+    numbers.add(4);
+    numbers.add(2);
+    System.out.println();
+    System.out.println("Os valores adicionados no array foram");
+    numbers.forEach(n -> System.out.println(n));
+  }
+
+  public void getEqualityDiferencePairs(int target) {
+    verify = 0;
+    numbers.forEach(n -> {
+      numbers.forEach(nChkec -> {
+        if ((n - nChkec) == target) {
+          verify += 1;
+        }
+      });
+    });
+    System.out.println();
+  }
 }
