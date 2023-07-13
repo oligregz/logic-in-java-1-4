0f62b073f3f2af22dc9d5b3e6e71863a696b98a9
oligregz <gregorysak90@gmail.com>
2023-07-12 21:39:19 -0300

    feat: second challenge implemented

--- src/supera_challengers_1_4/Application.java
+++ src/supera_challengers_1_4/Application.java
@@ -1,6 +1,7 @@
 package supera_challengers_1_4;
 
 import supera_challengers_1_4.first_challenger.OddPairs;
+import supera_challengers_1_4.second_challenger.ValueBanknotes;
 
 public class Application {
 
@@ -8,11 +9,16 @@ public class Application {
 
     /* First challenge */
     System.out.println("--------------------------------------------------------------");
-    System.out.println("1º)");    
+    System.out.println("1º)");
     OddPairs oddPairs = new OddPairs();
     oddPairs.setDefaultNumbers();
     oddPairs.getPairsBeforeOdd();
 
+    /* Second challenge */
     System.out.println("--------------------------------------------------------------");
+    System.out.println("2º)");
+    ValueBanknotes valueBanknotes = new ValueBanknotes();
+    valueBanknotes.setAmount(576.73);
+    valueBanknotes.printNotesAndCoins();
   }
 }
--- src/supera_challengers_1_4/second_challenger/ValueBanknotes.java
+++ src/supera_challengers_1_4/second_challenger/ValueBanknotes.java
@@ -2,4 +2,39 @@ package supera_challengers_1_4.second_challenger;
 
 public class ValueBanknotes {
 
+  protected double amount;
+  protected int[] notes = { 100, 50, 20, 10, 5, 2 };
+  protected double[] coins = { 1, 0.50, 0.25, 0.10, 0.05, 0.01 };
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(double amount) {
+    this.amount = amount;
+  }
+
+  public void printNotes() {
+    System.out.println("NOTAS:");
+    for (int note : notes) {
+      int quantidadeNotas = (int) (amount / note);
+      System.out.printf("%d nota(s) de R$ %.2f%n", quantidadeNotas, (double) note);
+      amount %= note;
+    }
+  }
+
+  public void printCoins() {
+    System.out.println("MOEDAS:");
+    for (double coin : coins) {
+      int quantidadeMoedas = (int) (amount / coin);
+      System.out.printf("%d moeda(s) de R$ %.2f%n", quantidadeMoedas, coin);
+      amount %= coin;
+    }
+  }
+
+  public void printNotesAndCoins() {
+    System.out.println();
+    printNotes();
+    printCoins();
+  }
 }
